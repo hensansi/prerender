bb3f2753cfb645383287b9e5ddfe8645a00b32d6
Henrique Silva <hensansi@gmail.com>
2017-08-17 08:56:57 +0200

    Add local chromium to the dependencies

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
+var glob = require('glob');
+var chromeLocation = glob.sync('./node_modules/puppeteer/.local-chromium/linux-*/chrome-linux/chrome').pop();
 
-var server = prerender();
+var server = prerender({chromeLocation});
 
 server.use(prerender.sendPrerenderHeader());
 server.use(prerender.blockResources());
